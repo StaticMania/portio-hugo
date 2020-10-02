a8cd30f1d617dae82926f89f1bfca4ab3e16ff7a
Aaron N. Brock <Contact@AaronNBrock.com>
2020-10-02 18:54:48 -0400

    fix issue 12

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,17 +158,27 @@ $(document).ready(function () {
   });
 
   // Modal Popup
-  $(".popup-button").magnificPopup({
-    type: "iframe",
-    iframe: {
-      patterns: {
-        youtube: {
-          index: "youtube.com/",
-          id: "v=",
-          src: "//www.youtube.com/embed/tgbNymZ7vqY",
-        },
-      },
-    },
+  // $(".popup-button").magnificPopup({
+  //   type: "iframe",
+  //   iframe: {
+  //     patterns: {
+  //       youtube: {
+  //         index: "youtube.com/",
+  //         id: "v=",
+  //         src: "//www.youtube.com/embed/tgbNymZ7vqY",
+  //       },
+  //     },
+  //   },
+  // });
+
+  $('.popup-button').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
   });
 
   $(".portfolio-item-grid").masonry({
